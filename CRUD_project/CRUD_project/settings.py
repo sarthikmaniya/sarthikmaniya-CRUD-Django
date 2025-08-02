885ea8f0455e86ab994aa1e1c6958169298b447d
Sarthik Maniya <sarthikmaniya123@gmail.com>
2025-08-02 20:07:40 +0530

    save1

--- CRUD_project/CRUD_project/settings.py
+++ CRUD_project/CRUD_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o0kbx9t^)5z1))igl*6t*t()0$w3y#4gbh2!ud0)06s*-4u7$8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = []
